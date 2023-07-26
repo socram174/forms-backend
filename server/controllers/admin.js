4f137d4e74c60ced30aaab63adb1b451cf9d30db
socram174 <socram174@hotmail.com>
2023-07-26 14:32:54 -0400

    Fixed admin information editing

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -4,9 +4,10 @@ import Admin from '../models/Admin.js';
 export const editInformation = async (req,res) => {
     try{
         const adminId = req.params.id;
-        const {name,email} = req.body;
+        const {name, email, token} = req.body;
         console.log(name);
         console.log(email);
+        console.log(token);
         const admin = await Admin.findById(adminId);
         if(!admin) return res.status(400).json({ msg: "User does not exist."});
 
@@ -14,7 +15,7 @@ export const editInformation = async (req,res) => {
         admin.name = name;
         admin.email = email;
         admin.save();
-        res.status(200).json(admin);
+        res.status(200).json({admin,token});
     } catch (err) {
         res.status(500).json({error: err.message});
     }
--- server/models/Admin.js
+++ server/models/Admin.js
@@ -14,7 +14,9 @@ const AdminSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    
 },
+{timestamps: true}
 );
 
 const Admin = mongoose.model("Admin",AdminSchema);
